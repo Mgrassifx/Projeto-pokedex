5e7c83cf83e09ae7173d307103b57a7cdd699976
Marco <marcooo0034h@gmail.com>
2026-09-05 13:46:10 -0300

    Fit: Impleta as pastas detalhe e pokemon

---
+++ pokedex/app/layout.tsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+
+export default function LayoutProps({children}) {
+
+    return (
+
+        <html lang="pt-br">
+            <body>
+                {children}  
+            </body>
+        </html>
+    );
+}
